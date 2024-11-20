c79483f2634b90ed636f4391bf884b4cc6e70a4b
Park Lu <chochinlu@gmail.com>
2024-11-20 16:31:11 +0800

    update

--- db/table_view.sql
+++ db/table_view.sql
@@ -42,6 +42,8 @@ select
   ld.current_status as "現況",
   ld.vacancy_rate as "空置比例(%)",
   ld.note as "備註",
+  a.coordinates::text as "定位座標",
+  a.area_coordinates::text as "區域座標組",
   a.status as "狀態",
   a.created_at as "建立時間"
 from
@@ -59,13 +61,12 @@ order by
   a.section,
   ld.lot_number;
 
-create view
-  public.test_idle_building_assets_view as
-select
+CREATE OR REPLACE VIEW public.test_idle_building_assets_view AS
+SELECT
   a.id as "資產ID",
   bd.id as "建物明細ID",
-  a.target_name as "標的名稱",
   d.name as "行政區",
+  a.target_name as "標的名稱",
   a.section as "地段",
   a.address as "地址",
   ag.name as "管理機關",
@@ -80,23 +81,25 @@ select
   string_agg(distinct bld.lot_number::text, ', '::text) as "座落地號",
   string_agg(distinct bld.land_type::text, ', '::text) as "土地類型",
   string_agg(distinct bld.land_manager::text, ', '::text) as "土地管理者",
+  a.coordinates::text as "定位座標",        -- 使用 ::text 轉換
+  a.area_coordinates::text as "區域座標組",  -- 使用 ::text 轉換
   a.status as "活化狀態",
   a.created_at as "建立時間"
-from
+FROM
   test_assets a
-  left join test_districts d on a.district_id = d.id
-  left join test_agencies ag on a.agency_id = ag.id
-  left join test_building_details bd on a.id = bd.asset_id
-  left join test_building_land_details bld on a.id = bld.asset_id
-where
+  LEFT JOIN test_districts d ON a.district_id = d.id
+  LEFT JOIN test_agencies ag ON a.agency_id = ag.id
+  LEFT JOIN test_building_details bd ON a.id = bd.asset_id
+  LEFT JOIN test_building_land_details bld ON a.id = bld.asset_id
+WHERE
   a.status::text = '未活化'::text
-  and a.type::text = '建物'::text
-  and bd.deleted_at is null
-group by
+  AND a.type::text = '建物'::text
+  AND bd.deleted_at IS NULL
+GROUP BY
   a.id,
   bd.id,
-  a.target_name,
   d.name,
+  a.target_name,
   a.section,
   a.address,
   ag.name,
@@ -108,9 +111,11 @@ group by
   bd.current_status,
   bd.vacancy_rate,
   bd.note,
+  a.coordinates::text,        -- GROUP BY 也要使用 ::text
+  a.area_coordinates::text,   -- GROUP BY 也要使用 ::text
   a.status,
   a.created_at
-order by
+ORDER BY
   a.id,
   d.name,
   a.section,
